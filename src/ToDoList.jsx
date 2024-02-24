f7969eaf25f34fa6e3e2cdcba4590462f89a4b5d
thushanm <122750947+thushanm@users.noreply.github.com>
2024-02-25 00:36:44 +0530

    making funtion

---
+++ src/ToDoList.jsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import {findDOMNode} from "react-dom";
+
+export function ToDoList(){
+const [task,setTask]=useState([])
+    const [newTask,setNewTask] = useState("")
+
+    function handleInputChange(){
+
+    }
+    function addTask(){
+
+    }
+    function deleteTask(){
+
+    }
+}
